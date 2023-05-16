1f9c7152bcfbd4bd569272efcfa3ce22d42dc70e
CesarWebTrin <cesinhart@gmail.com>
2023-05-16 19:38:02 -0300

    SECOND COMMIT PORT 8080 VM COMPLETED

--- network.tf
+++ network.tf
@@ -35,6 +35,18 @@ resource "azurerm_network_security_group" "cloudaut-sgp" {
     destination_address_prefix = "*"
   }
 
+   security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = {
     environment = "Production"
   }
